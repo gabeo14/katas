d3e64b8b47e19cce7bd0de12c16f0e1d2d147ff9
Gabe Oleinik <gomusic14@gmail.com>
2021-10-26 11:48:15 -0400

    Starting again. First HR python Kata

---
+++ HR_python3/wierdnotweird.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if n % 2 == 1:
+        print('Weird')
+    elif 2 <= n <= 5 or n > 20:
+        print('Not Weird')
+    else:
+        print('Weird')
